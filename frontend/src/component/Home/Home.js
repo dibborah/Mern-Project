13671b326d9b564a9710bfb407b8fdbd9bc1d890
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-14 19:08:13 +0530

    Products page created along with designs

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,6 +1,14 @@
 import React, { Fragment } from 'react'
 import { CiDesktopMouse2 } from "react-icons/ci";
 import './Home.css'
+import Product from './Product.js'
+
+const product = {
+  name: 'Blue Tshirt',
+  images: [{ url: 'https://i.ibb.co/DRST11n/1.webp'}],
+  price: '₹3000',
+  _id: 'abhishek',
+}
 
 const Home = () => {
   return (
@@ -14,6 +22,20 @@ const Home = () => {
             Scroll <CiDesktopMouse2 />
           </button>
         </a>
+      </div>
+
+      <h2 className="homeHeading">Featured Products</h2>
+
+      <div className="container" id="container">
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
       </div>
     </Fragment>
   )
