cdbfae7eb9df41903a9db52673b2f90b3d24235d
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-14 17:06:22 +0530

    Footer design is ready as of now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import Header from './component/layout/Header.js'
+import Header from './component/layout/Header/Header.js'
 import { BrowserRouter as Router } from 'react-router-dom'
+import WebFont from 'webfontloader'
+import Footer from './component/layout/Footer/Footer.js'
+
+
 
 const App = () => {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Roboto', 'Droid Sans', 'Chilanka']
+        // families: ['Droid Sans', 'Chilanka']
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <Header />
+
+
+      <Footer />
     </Router>
   )
 }
