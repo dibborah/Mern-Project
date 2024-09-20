d048a34574ab8ab6432545bb8521e146cd0427b2
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-20 21:09:32 +0530

    basic server is ready

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const app = require("./app")
 
 const dotenv = require("dotenv");
+const connectDatabase = require('./config/database');
 
 // Config
 
-dotenv.config({path:"backend/config/config.env"});// connecting to the config.env file
+dotenv.config({path:"backend/config/config.env"}); // connecting to the config.env file
+
+// Connecting to database
+connectDatabase();
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
-})
+});
