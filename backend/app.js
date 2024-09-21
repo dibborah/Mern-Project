27bf2a97ca27c4e9c6b7e75ec59a58b2f917c1bd
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-21 16:24:40 +0530

    For error handling middlewares created

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const errorMiddleware = require('./middleware/error');
+
 // basic env which we will use
 app.use(express.json())
 
@@ -8,5 +10,9 @@ app.use(express.json())
 const product = require("./routes/productRoute");
 
 app.use("/api/v1", product);
+
+// Middleware for Errors
+
+app.use(errorMiddleware);
 
 module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorhandler");
 
 
 // Create Product -- Admin
@@ -29,10 +30,11 @@ exports.getProductDetails = async(req, res, next) => {
     let product = await Product.findById(req.params.id);
 
     if(!product) {
-        return res.status(500).json({
-            success: false,
-            message: "Product not found"
-        })
+        // return res.status(500).json({
+        //     success: false,
+        //     message: "Product not found"
+        // })
+        return next(new ErrorHandler('Product not found', 404));
     }
 
     res.status(200).json({
