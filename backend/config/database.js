038ac95ff94310f6cc910b5ae70bd80901f8e31e
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-21 17:14:51 +0530

    handled unHandledRejection error and unCaught type exception error

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,10 +9,7 @@ const connectDatabase = () => {
         useCreateIndex: true,
     }).then((data) => {
         console.log(`Mongodb connected with server: ${data.connection.host}`);
-    }).catch((error) => {
-        console.log(error);
     });
-
 };
 
 module.exports = connectDatabase;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const app = require("./app")
 const dotenv = require("dotenv");
 const connectDatabase = require('./config/database');
 
+// Handling Uncaught Exception
+process.on('uncaughtException', (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log('Shutting down the server due to Uncaught Exception');
+    process.exit(1);
+})
+
 // Config
 
 dotenv.config({path:"backend/config/config.env"}); // connecting to the config.env file
@@ -11,6 +18,19 @@ dotenv.config({path:"backend/config/config.env"}); // connecting to the config.e
 connectDatabase();
 
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
 });
+
+// console.log(youtube)// since youtube is not defined, an error will be thrown
+// this error is called uncaught type error
+
+// Unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log('Shutting down the server due to unhandled promise rejection');
+
+    server.close(() => {
+        process.exit(1);
+    })
+})
