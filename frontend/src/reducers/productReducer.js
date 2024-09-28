56adb604bb07a1dc965e6ad706e79dd3153f7fc3
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-28 17:42:58 +0530

    productReducer plus product new constants added

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -2,6 +2,9 @@ import {
     All_PRODUCT_REQUEST,
     All_PRODUCT_SUCCESS,
     All_PRODUCT_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
     CLEAR_ERRORS,
 } from '../constants/productConstants.js'
 
@@ -19,6 +22,33 @@ export const productReducer = (state = { products: [] }, action) => {
             productsCount: action.payload.productsCount,
           }
         case All_PRODUCT_FAIL: 
+        return {
+            loading: false,
+            error:action.payload,
+          }
+        case CLEAR_ERRORS: 
+        return {
+            ...state,
+            error:null,
+          }
+        default:
+          return state;
+    }
+}
+
+export const productDetailsReducer = (state = { products: {} }, action) => {
+    switch(action.type) {
+        case PRODUCT_DETAILS_REQUEST: 
+          return {
+            loading: true,
+            ...state,
+          }
+        case PRODUCT_DETAILS_SUCCESS: 
+        return {
+            loading: false,
+            products: action.payload,
+          }
+        case PRODUCT_DETAILS_FAIL: 
         return {
             loading: false,
             error:action.payload,
