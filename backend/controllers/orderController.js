6786029f71535d25e1a8e0bb274d2fe44b6668d9
dibya.borah <dibyajyotiborah469@gmail.com>
2024-09-28 15:18:15 +0530

    done update order along with updateStock and deleteOrder along with routes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,21 +79,52 @@ exports.getAllOrders = catchAsyncErrors(async(req, res, next) => {
 });
 
 
-// Need to fix this below function
 // update Order Status -- Admin
-exports.updateOrder = catchAsyncErrors(async(req, res, next) => {
-    const order = await Order.find(req.params.id);
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
 
     if(order.orderStatus === 'Delivered') {
         return next(new ErrorHandler("You have already delivered this order", 400));
     }
 
-    order.orderItems.forEach(order => {
-        await updateStock(order.Product, order.quantity);
-    })
+    order.orderItems.forEach(async (o) => {
+       await updateStock(o.product, o.quantity);
+    });
+
+    order.orderStatus = req.body.status;
+
+    if(req.body.status === 'Delivered') {
+        order.deliveredAt = Date.now()
+    }
+
+    await order.save({ validateBeforeSave: false });
 
     res.status(200).json({
         success: true,
-        order
+    });
+});
+
+
+async function updateStock(id, quantity) {
+    const product = await Product.findById(id)
+
+    product.Stock -= quantity;
+
+    await product.save({ validateBeforeSave: false });
+}
+
+
+// delete Order -- Admin
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+
+    if(!order) {
+        return next (new ErrorHandler("Order not found with this Id"));
+    }
+
+    await order.remove();
+    
+    res.status(200).json({
+        success: true,
     });
 });
