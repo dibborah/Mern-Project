bea999c292c9c42b3df6d70f88fe77a38b307a72
dibya.borah <dibyajyotiborah469@gmail.com>
2024-10-02 16:03:09 +0530

    pagination functionality done

--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -8,53 +8,59 @@ import { useParams } from "react-router-dom";
 import Pagination from 'react-js-pagination';
 
 const Products = () => {
-    const dispatch = useDispatch();
-    const params = useParams();
-    const [currentPage, setCurrentPage] = useState(1);
-    const { products, loading, error, productsCount, resultPerPage } = useSelector(state => state.products);
-    const setCurrentPageNo = (e) => {
-      setCurrentPage(e);
-    }
-    useEffect(() => {
-        dispatch(getProduct(params?.keyword));
-    }, [dispatch]);
-    return (
-        <Fragment>
-            {loading ? <Loader />
-                : (
-                    <Fragment>
-                      <h2 className="productsHeading">Products</h2>
+  const dispatch = useDispatch();
+  const params = useParams();
+  const [currentPage, setCurrentPage] = useState(1);
+  const { products, loading, error, productsCount, resultPerPage } = useSelector(state => state.products);
+  const setCurrentPageNo = (e) => {
+    setCurrentPage(e);
+  }
+  const keyword = params?.keyword;
+  useEffect(() => {
+    dispatch(getProduct(keyword, currentPage));
+  }, [dispatch, currentPage, keyword]);
+  return (
+    <Fragment>
+      {loading ? <Loader />
+        : (
+          <Fragment>
+            <h2 className="productsHeading">Products</h2>
 
-                        <div className="products">
-                          {
-                            products && 
-                              products.map((product) => (
-                                <ProductCard product={product} key={product._id} />
-                              ))
-                          }
-                        </div>        
-                        
-                        <div className="paginationBox">
-                          <Pagination
-                            activePage={currentPage}
-                            itemsCountPerPage={resultPerPage}
-                            totalItemsCount={productsCount}
-                            onChange={setCurrentPageNo}
-                            nextPageText="Next"
-                            prevPageText="Prev"
-                            firstPageText="1st"
-                            lastPageText="Last"
-                            itemClass="page-item"
-                            linkClass="page-link"
-                            activeClass="pageItemActive"
-                            activeLinkClass="pageLinkActive"
-                          />
-                        </div>
-                    </Fragment>
-                )
+            <div className="products">
+              {
+                products &&
+                products.map((product) => (
+                  <ProductCard product={product} key={product._id} />
+                ))
+              }
+            </div>
+
+            {
+              resultPerPage < productsCount && (
+                <div className="paginationBox">
+                  <Pagination
+                    activePage={currentPage}
+                    itemsCountPerPage={resultPerPage}
+                    totalItemsCount={productsCount || 1}
+                    onChange={setCurrentPageNo}
+                    nextPageText="Next"
+                    prevPageText="Prev"
+                    firstPageText="1st"
+                    lastPageText="Last"
+                    itemClass="page-item"
+                    linkClass="page-link"
+                    activeClass="pageItemActive"
+                    activeLinkClass="pageLinkActive"
+                  />
+                </div>
+              )
             }
-        </Fragment>
-    )
+
+          </Fragment>
+        )
+      }
+    </Fragment>
+  )
 }
 
 export default Products;
